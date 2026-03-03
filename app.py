47c244007b886d770f0381f788611ad85acfc16f
DollyKabamba <119793681+DollyKabamba@users.noreply.github.com>
2026-03-03 11:34:02 +0000

    Modif pour test déploiement sur Render

--- app.py
+++ app.py
@@ -967,16 +967,4 @@ def e500(e): return render_template('404.html', error=True), 500
 if __name__ == '__main__':
     init_db(); app.run(debug=True, host='0.0.0.0', port=5000)
 
-###
-import os
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello Render 🚀"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
